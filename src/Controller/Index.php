3dd8fdaf80fe5829cd1f9a6dc6a03a44c26a6487
Luis A. Martín <lorgam@gmail.com>
2020-08-24 01:28:13 +0200

    Formulario para continuar partida

--- src/Controller/Index.php
+++ src/Controller/Index.php
@@ -4,14 +4,40 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use App\Entity\Game;
 
 class Index extends AbstractController
 {
   /**
    * @Route("/", name="index")
    */
-  public function index()
+  public function index(Request $request)
   {
-    return $this->render('index.html.twig');
+    $form = $this->createFormBuilder()
+                 ->add('id', NumberType::class, ['html5' => true])
+                 ->add('continue', SubmitType::class, ['label' => 'Continuar'])
+                 ->getForm();
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid())
+    {
+      $formData = $form->getData();
+      $id = $formData['id'];
+      $game = $this->getDoctrine()->getRepository(Game::class)->find($id);
+
+      if (!$game) {
+        throw $this->createNotFoundException('Partida no encontrada: '.$id);
+      }
+
+      return $this->redirectToRoute('continue', ['id' => $id]);
+    }
+
+    return $this->render('index.html.twig', [
+      'form' => $form->createView()
+    ]);
   }
 }
